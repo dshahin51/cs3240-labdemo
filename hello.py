875dae9378b67db419ca1f1ae324c178f1dcaccc
Haitham Shahin <hms3nf@virginia.edu>
2015-09-28 14:43:47 -0400

    Added the new_file

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
+from new_file import another_function
 from helper import greeting
 
 if __name__ == "__main__":
 	greeting('hello')
+	another_function()
---
+++ new_file.py
@@ -0,0 +1,2 @@
+def another_function():
+	print("I'm in the new file made in the developer branch")
